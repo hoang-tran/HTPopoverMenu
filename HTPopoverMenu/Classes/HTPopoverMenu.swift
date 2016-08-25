8834a331a974e69d3003420c91984ae504b906f2
Hoang Tran <hoangtx.master@gmail.com>
2016-08-25 22:27:16 +0700

    add popoverContainer with menuTableView

--- HTPopoverMenu/Classes/HTPopoverMenu.swift
+++ HTPopoverMenu/Classes/HTPopoverMenu.swift
@@ -10,42 +10,102 @@ import UIKit
 
 public class HTPopoverMenu : UIViewController {
 
+  var menuHeight: CGFloat = 200
+
   public override func loadView() {
     super.loadView()
     self.view.addSubview(darkView)
+    self.view.addSubview(popoverContainerView)
   }
 
   lazy var darkView: UIView = {
-    let screenSize = UIScreen.mainScreen().bounds.size
-    let view = UIView(frame: CGRect(x: 0, y: 0, width: screenSize.width, height: screenSize.height))
+    let view = UIView(frame: CGRect(x: 0, y: 0, width: self.window.bounds.size.width, height: self.window.bounds.size.height))
     view.backgroundColor = UIColor.blackColor()
     view.layer.opacity = 0.5
-    view.translatesAutoresizingMaskIntoConstraints = true
-    view.autoresizingMask = [.FlexibleLeftMargin, .FlexibleRightMargin, .FlexibleTopMargin, .FlexibleBottomMargin, .FlexibleWidth, .FlexibleHeight]
     view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(onTapDarkView)))
     return view
   }()
 
+  lazy var popoverContainerView: UIView = {
+    let view = UIView(frame: CGRect(x: 0, y: 0, width: self.window.bounds.size.width, height: self.menuHeight))
+    view.backgroundColor = UIColor.whiteColor()
+    self.menuTableView.frame.size = view.bounds.size
+    view.addSubview(self.menuTableView)
+    return view
+  }()
+
+  lazy var menuTableView: UITableView = {
+    let tableView = UITableView(frame: CGRectZero, style: .Plain)
+    tableView.bounces = false
+    tableView.dataSource = self
+    tableView.delegate = self
+    return tableView
+  }()
+
+  lazy var window: UIWindow = {
+    return UIApplication.sharedApplication().keyWindow!
+  }()
 }
 
 // public interface
 extension HTPopoverMenu {
-
   public func showFromView(view: UIView) {
-    UIApplication.sharedApplication().keyWindow?.addSubview(self.view)
+    window.addSubview(self.view)
+    updatePopoverPositionFromView(view)
   }
 
   public func hide() {
     self.view.removeFromSuperview()
   }
-
 }
 
 // event handling
 extension HTPopoverMenu {
-
   func onTapDarkView(tapGesture: UITapGestureRecognizer) {
     hide()
   }
+}
 
+let menuCellReuseID = "menuCellReuseID"
+
+extension HTPopoverMenu : UITableViewDataSource {
+  public func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    return 1
+  }
+
+  public func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 4
+  }
+
+  public func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    var cell = tableView.dequeueReusableCellWithIdentifier(menuCellReuseID)
+    if cell == nil {
+      cell = UITableViewCell(style: .Default, reuseIdentifier: menuCellReuseID)
+    }
+    cell?.textLabel!.text = String(indexPath.row)
+    return cell!
+  }
+}
+
+extension HTPopoverMenu : UITableViewDelegate {
+  public func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+    return 50
+  }
+
+  public func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+    tableView.deselectRowAtIndexPath(indexPath, animated: true)
+  }
+}
+
+// helper methods
+extension HTPopoverMenu {
+  func updatePopoverPositionFromView(view: UIView) {
+    let viewOriginInWorldCoordinate = view.convertPoint(CGPointZero, toView: window)
+    let haveEnoughSpaceAtBottom = (viewOriginInWorldCoordinate.y + view.bounds.size.height + popoverContainerView.bounds.size.height) <= window.bounds.size.height
+    if haveEnoughSpaceAtBottom {
+      popoverContainerView.frame.origin.y = viewOriginInWorldCoordinate.y + view.bounds.size.height
+    } else {
+      popoverContainerView.frame.origin.y = viewOriginInWorldCoordinate.y - popoverContainerView.bounds.size.height
+    }
+  }
 }
